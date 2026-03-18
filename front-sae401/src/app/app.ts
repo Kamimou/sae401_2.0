c3d68d0761e96a31aa78f339c53a83948ec1489d
Kamimou <imenelacha@gmail.com>
2026-03-18 22:11:52 +0100

    correction router

--- front-sae401/src/app/app.ts
+++ front-sae401/src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { AccueilComponent } from './accueil/accueil.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [CommonModule, RouterModule, AccueilComponent, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterModule, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
